66a4f0814f9e241ca6f24d8361680eda29eea9ab
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2016-04-30 12:47:44 +0900

    リファクタリング

--- src/main/java/logbook/pushbullet/Pusher.java
+++ src/main/java/logbook/pushbullet/Pusher.java
@@ -26,29 +26,25 @@ public class Pusher {
      * @param message String
      */
     public void pushToSelectedTargets(String title, String message) {
-        PushbulletService service = ServiceFactory.getService(accessToken);
+        PushbulletService service = ServiceFactory.create(accessToken);
 
         DeviceCollection.get()
                 .stream()
                 .filter(Device::isActive)
                 .filter(Device::isSelected)
-                .forEach(device -> {
-                    service.push(PushParameter.noteToDevice(device, title, message))
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(JavaFxScheduler.getInstance())
-                            .subscribe(result -> LoggerHolder.LOG.debug(result.getPushes()), LoggerHolder.LOG::error);
-                });
+                .forEach(device -> service.push(PushParameter.noteToDevice(device, title, message))
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(JavaFxScheduler.getInstance())
+                        .subscribe(result -> LoggerHolder.LOG.debug(result.getPushes()), LoggerHolder.LOG::error));
 
         ChannelCollection.get()
                 .stream()
                 .filter(Channel::isActive)
                 .filter(Channel::isSelected)
-                .forEach(channel -> {
-                    service.push(PushParameter.noteToChannel(channel, title, message))
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(JavaFxScheduler.getInstance())
-                            .subscribe(result -> LoggerHolder.LOG.debug(result.getPushes()), LoggerHolder.LOG::error);
-                });
+                .forEach(channel -> service.push(PushParameter.noteToChannel(channel, title, message))
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(JavaFxScheduler.getInstance())
+                        .subscribe(result -> LoggerHolder.LOG.debug(result.getPushes()), LoggerHolder.LOG::error));
     }
 
     private static class LoggerHolder {

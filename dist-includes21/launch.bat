af8bf0e036adb38930e7f38302bcb51db97393cf
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2024-10-29 21:14:56 +0900

    Java 17,21のWindows版起動スクリプトに-Djavafx.allowjs=trueを追加

---
+++ dist-includes21/launch.bat
@@ -0,0 +1,2 @@
+SET JVM_OPT=-XX:MaxMetaspaceSize=256M
+START javaw %JVM_OPT% -Djavafx.allowjs=true -jar logbook-kai.jar
--- scripts/launch-java17.bat
+++ scripts/launch-java17.bat
@@ -1,4 +1,4 @@
 SET JAVA_HOME="C:\Program Files\Zulu\zulu-17"
 SET PATH=%JAVA_HOME%\bin;%PATH%
 SET JVM_OPT=-XX:MaxMetaspaceSize=256M
-START javaw.exe %JVM_OPT% -jar logbook-kai.jar
+START javaw.exe %JVM_OPT% -Djavafx.allowjs=true -jar logbook-kai.jar

4b83539d868b4289ce01b58133d8bf3ca0c20c4d
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2024-10-31 21:47:09 +0900

    support XDG_*_HOME env

--- src/main/java/logbook/internal/AppPath.java
+++ src/main/java/logbook/internal/AppPath.java
@@ -7,19 +7,31 @@ import java.nio.file.Paths;
  */
 public final class AppPath {
     /**
-     * ユーザーのホームディレクトリ
-     */
-    public static final String HOME_DIR = System.getProperty("user.home"); //$NON-NLS-1$
-
-    /**
      * 設定が保存されるディレクトリ
-     * XDG Base Directory XDG_CONFIG_HOMEのデフォルト値 ~/.config 以下に保存します
      */
-    public static final String CONFIG_DIR = Paths.get(HOME_DIR,".config/logbook-kai").toAbsolutePath().toString(); //$NON-NLS-1$
+    public static final String CONFIG_DIR;
 
     /**
      * データが保存されるディレクトリ
-     * XDG Base Directory XDG_DATA_HOMEのデフォルト値 ~/.local/share 以下に保存します
      */
-    public static final String DATA_DIR = Paths.get(HOME_DIR,".local/share/logbook-kai").toAbsolutePath().toString(); //$NON-NLS-1$
+    public static final String DATA_DIR;
+
+    static {
+        final String APP_NAME = "logbook-kai"; //$NON-NLS-1$
+        final String HOME_DIR = System.getProperty("user.home"); //$NON-NLS-1$
+        final String XDG_CONFIG_HOME = System.getenv("XDG_CONFIG_HOME"); //$NON-NLS-1$
+        final String XDG_DATA_HOME = System.getenv("XDG_DATA_HOME"); //$NON-NLS-1$
+
+        if (XDG_CONFIG_HOME != null) {
+            CONFIG_DIR = Paths.get(XDG_CONFIG_HOME, APP_NAME).toAbsolutePath().toString();
+        } else {
+            CONFIG_DIR = Paths.get(HOME_DIR, ".config", APP_NAME).toAbsolutePath().toString(); //$NON-NLS-1$
+        }
+
+        if (XDG_DATA_HOME != null) {
+            DATA_DIR = Paths.get(XDG_DATA_HOME, APP_NAME).toAbsolutePath().toString();
+        } else {
+            DATA_DIR = Paths.get(HOME_DIR, ".local/share", APP_NAME).toAbsolutePath().toString(); //$NON-NLS-1$
+        }
+    }
 }

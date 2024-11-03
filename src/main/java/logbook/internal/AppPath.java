8f3ecc4fbdd93483cff94cba79337b3ce61a30ec
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2024-11-03 10:18:45 +0900

    update AppPath.java

--- src/main/java/logbook/internal/AppPath.java
+++ src/main/java/logbook/internal/AppPath.java
@@ -17,19 +17,19 @@ public final class AppPath {
     public static final String DATA_DIR;
 
     static {
-        final String APP_NAME = "logbook-kai"; //$NON-NLS-1$
-        final String HOME_DIR = System.getProperty("user.home"); //$NON-NLS-1$
-        final String LOGBOOK_KAI_CONFIG_DIR = System.getenv("LOGBOOK_KAI_CONFIG_DIR"); //$NON-NLS-1$
-        final String LOGBOOK_KAI_DATA_DIR = System.getenv("LOGBOOK_KAI_DATA_DIR"); //$NON-NLS-1$
-        final String XDG_CONFIG_HOME = System.getenv("XDG_CONFIG_HOME"); //$NON-NLS-1$
-        final String XDG_DATA_HOME = System.getenv("XDG_DATA_HOME"); //$NON-NLS-1$
+        final String APP_NAME = "logbook-kai";
+        final String HOME_DIR = System.getProperty("user.home");
+        final String LOGBOOK_KAI_CONFIG_DIR = System.getProperty("logbook_kai.config_dir");
+        final String LOGBOOK_KAI_DATA_DIR = System.getProperty("logbook_kai.data_dir");
+        final String XDG_CONFIG_HOME = System.getenv("XDG_CONFIG_HOME");
+        final String XDG_DATA_HOME = System.getenv("XDG_DATA_HOME");
 
         if (LOGBOOK_KAI_CONFIG_DIR != null) {
             CONFIG_DIR = LOGBOOK_KAI_CONFIG_DIR;
         } else if (XDG_CONFIG_HOME != null) {
             CONFIG_DIR = Paths.get(XDG_CONFIG_HOME, APP_NAME).toAbsolutePath().toString();
         } else {
-            CONFIG_DIR = Paths.get(HOME_DIR, ".config", APP_NAME).toAbsolutePath().toString(); //$NON-NLS-1$
+            CONFIG_DIR = Paths.get(HOME_DIR, ".config", APP_NAME).toAbsolutePath().toString();
         }
 
         if (LOGBOOK_KAI_DATA_DIR != null) {
@@ -37,7 +37,7 @@ public final class AppPath {
         } else if (XDG_DATA_HOME != null) {
             DATA_DIR = Paths.get(XDG_DATA_HOME, APP_NAME).toAbsolutePath().toString();
         } else {
-            DATA_DIR = Paths.get(HOME_DIR, ".local/share", APP_NAME).toAbsolutePath().toString(); //$NON-NLS-1$
+            DATA_DIR = Paths.get(HOME_DIR, ".local/share", APP_NAME).toAbsolutePath().toString();
         }
     }
 }

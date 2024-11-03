d4baba1f352fbbf7819191745c5f6aeeeb905925
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2024-11-04 08:48:30 +0900

    Add JVM options --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED

--- dist-includes/launch.bat
+++ dist-includes/launch.bat
@@ -1,2 +1,2 @@
-SET JVM_OPT=-XX:MaxMetaspaceSize=256M
+SET JVM_OPT="-XX:MaxMetaspaceSize=256M --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED"
 START javaw %JVM_OPT% -Djavafx.allowjs=true -Dlogbook_kai.config_dir=.\config -Dlogbook_kai.data_dir=. -jar logbook-kai.jar
--- dist-includes/launch.sh
+++ dist-includes/launch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-JVM_OPT=-XX:MaxMetaspaceSize=256M
+JVM_OPT="-XX:MaxMetaspaceSize=256M --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED"
 java $JVM_OPT -Djavafx.allowjs=true -Dlogbook_kai.config_dir=./config -Dlogbook_kai.data_dir=. -jar logbook-kai.jar

cebff6451e58ed26401c366106487baf5865018e
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2024-11-03 13:30:58 +0900

    fix launch.bat

--- dist-includes/launch.bat
+++ dist-includes/launch.bat
@@ -1,2 +1,2 @@
 SET JVM_OPT=-XX:MaxMetaspaceSize=256M
-START javaw %JVM_OPT% -Djavafx.allowjs=true -jar -Dlogbook_kai.config_dir=./config -Dlogbook_kai.data_dir=. logbook-kai.jar
+START javaw %JVM_OPT% -Djavafx.allowjs=true -Dlogbook_kai.config_dir=.\config -Dlogbook_kai.data_dir=. -jar logbook-kai.jar

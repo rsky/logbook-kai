0a008ee8c2ed46706a968108de0205c4db6af6cf
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2025-10-23 00:53:30 +0900

    mitmproxy-javaの使用をやめ、自前でmitmdumpを起動する

---
+++ src/main/java/logbook/internal/proxy/MitmLauncher.java
@@ -0,0 +1,72 @@
+package logbook.internal.proxy;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+final public class MitmLauncher {
+    private final String mitmdumpPath;
+    private final int listenPort;
+    private final String listenHost;
+    private final int logbookPort;
+    private Process process = null;
+
+    public MitmLauncher(String mitmdumpPath, int listenPort, String listenHost, int logbookPort) {
+        this.mitmdumpPath = mitmdumpPath;
+        this.listenPort = listenPort;
+        this.listenHost = listenHost;
+        this.logbookPort = logbookPort;
+    }
+
+    public void start() throws IOException {
+        // python script file is zipped inside our jar. extract it into a temporary file.
+        final String pythonScriptPath = extractPythonScriptToFile();
+
+        final List<String> args = new ArrayList<>();
+        args.add(mitmdumpPath);
+        args.add("--anticache");
+        args.add("-q");
+        args.add("-p");
+        args.add(String.valueOf(listenPort));
+        if (listenHost != null) {
+            args.add("--listen-host");
+            args.add(listenHost);
+        }
+        args.add("-s");
+        args.add(pythonScriptPath);
+        args.add("--set");
+        args.add("logbook_port=" + logbookPort);
+
+        process = (new ProcessBuilder(args))
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start();
+    }
+
+    public void stop() {
+        if (process != null) {
+            process.destroy();
+            process = null;
+        }
+    }
+
+    /**
+     * This method is taken from mitmproxy-java
+     * https://github.com/appium/mitmproxy-java/blob/master/src/main/java/io/appium/mitmproxy/MitmproxyJava.java
+     */
+    private String extractPythonScriptToFile() throws IOException {
+        File outfile = File.createTempFile("mitmproxy-python-plugin", ".py");
+
+        try (
+                InputStream inputStream = getClass().getClassLoader().getResourceAsStream("scripts/proxy.py");
+                FileOutputStream outputStream = new FileOutputStream(outfile)) {
+
+            inputStream.transferTo(outputStream);
+        }
+
+        return outfile.getCanonicalPath();
+    }
+}
--- src/main/java/logbook/internal/proxy/ProxyServerImpl.java
+++ src/main/java/logbook/internal/proxy/ProxyServerImpl.java
@@ -1,7 +1,5 @@
 package logbook.internal.proxy;
 
-import io.appium.mitmproxy.InterceptedMessage;
-import io.appium.mitmproxy.MitmproxyJava;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -20,9 +18,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.BindException;
 import java.net.ServerSocket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * プロキシサーバーです。
@@ -31,104 +26,92 @@ import java.util.concurrent.ConcurrentHashMap;
 public final class ProxyServerImpl implements ProxyServerSpi {
     @Override
     public void run() {
-        final boolean usePassiveMode = AppConfig.get().isUsePassiveMode();
-        final boolean useMitmproxy = AppConfig.get().isUseMitmproxy();
-        if (usePassiveMode) {
-            if (useMitmproxy) {
-                showAlert("設定に問題があります", "設定でパッシブモードとmitmproxyの両方が有効になっています。mitmproxyは無視されます。", null);
-            }
-            this.runServer(true);
-        } else if (useMitmproxy) {
-            this.runServer(false);
-        } else {
-            showAlert("サーバーを起動できませんでした", "設定からパッシブモードかmitmproxyのどちらかを有効にしてください。", null);
-        }
-    }
-
-    private void runServer(boolean passiveMode) {
         // TCPポートが使用中かチェック
-        final int port = AppConfig.get().getListenPort();
-        if (!isPortAvailable(port)) {
-            showPortAlreadyUsed(port);
+        final int listenPort = AppConfig.get().getListenPort();
+        if (!isPortAvailable(listenPort)) {
+            showPortAlreadyUsed(listenPort);
             return;
         }
-        if (passiveMode) {
-            final int webSocketPort = 8765;
-            if (!isPortAvailable(webSocketPort)) {
-                showPortAlreadyUsed(webSocketPort);
-                return;
+
+        final boolean useMitmproxy = AppConfig.get().isUseMitmproxy();
+        // mitmproxyを使うとき、通信用に空きポートを確保
+        int internalPort = 0;
+        if (useMitmproxy) {
+            try {
+                internalPort = findAvailablePort();
+            } catch (IOException e){
+                LoggerHolder.get().warn("Exception occurred while searching for available port", e);
+                internalPort = 8765; // 便宜上fallback。通常は到達しない。
             }
         }
 
-        final Server server = new Server();
-        MitmproxyJava proxy = null;
+        this.runServer(listenPort, internalPort, useMitmproxy);
+    }
 
-        final ConcurrentHashMap<String, Integer> pidMap = new ConcurrentHashMap<>();
+    private void runServer(int listenPort, int internalPort, boolean useMitmproxy) {
+        final Server server = new Server();
+        final PassiveModeHandler passiveModeHandler = new PassiveModeHandler();
 
         try (final ServerConnector connector = new ServerConnector(server)) {
-            if (passiveMode) {
-                // 指定されたポートでパッシブモードのHTTPサーバが待ち受ける
-                connector.setPort(port);
+            if (useMitmproxy) {
+                connector.setPort(internalPort);
+                connector.setHost("localhost");
+            } else {
+                connector.setPort(listenPort);
                 if (AppConfig.get().isAllowOnlyFromLocalhost()) {
                     connector.setHost("localhost");
                 }
-                server.setConnectors(new Connector[]{connector});
-                server.setHandler(new PassiveModeHandler());
-            } else {
-                // 指定されたポートでmitmproxyが待ち受ける
-                // serverはproxyを終了させないようにjoin()するためにランダムなポートで待ち受ける
-                final List<String> extraMitmproxyParams = new ArrayList<>();
-                extraMitmproxyParams.add("--quiet");
+            }
+            server.setConnectors(new Connector[]{connector});
+            server.setHandler(passiveModeHandler);
+
+            MitmLauncher mitmLauncher = null;
+
+            if (useMitmproxy) {
+                String listenHost = null;
                 if (AppConfig.get().isAllowOnlyFromLocalhost()) {
-                    extraMitmproxyParams.add("--listen-host");
-                    extraMitmproxyParams.add("127.0.0.1");
+                    listenHost = "127.0.0.1";
                 }
-
-                final MitmMessageInterceptor interceptor = new MitmMessageInterceptor();
-
-                proxy = new MitmproxyJava(AppConfig.get().getMitmdumpPath(), (InterceptedMessage m) -> {
-                    // Windowsでは起動したmitmdumpの子プロセスが終了しない問題があるので、
-                    // ワークアラウンドとしてアドオンからPIDを送らせて、手動で始末する。
-                    if (m.getRequest().getMethod().equals("PID")) {
-                        pidMap.put("PID", m.getResponse().getStatusCode());
-                    } else {
-                        interceptor.intercept(m);
-                    }
-                    // レスポンスを改変しないのでnullを返す
-                    return null;
-                }, port, extraMitmproxyParams);
+                mitmLauncher = new MitmLauncher(AppConfig.get().getMitmdumpPath(), listenPort, listenHost, internalPort);
             }
 
             try {
                 try {
-                    if (proxy != null) {
-                        proxy.start();
-                    }
                     server.start();
+                    if (mitmLauncher != null) {
+                        mitmLauncher.start();
+                    }
                     server.join();
                 } finally {
-                    if (proxy != null) {
+                    if (mitmLauncher != null) {
                         try {
-                            if (pidMap.containsKey("PID")) {
-                                ProcessBuilder p = new ProcessBuilder("taskkill", "/F", "/PID", String.valueOf(pidMap.get("PID")));
-                                p.start().exitValue();
+                            if (passiveModeHandler.getMitmPid() > 0) {
+                                // Windowsのみ起動した孫プロセスが生き続ける問題があるので手動で始末する
+                                if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
+                                    try {
+                                        ProcessBuilder p = new ProcessBuilder("taskkill", "/F", "/PID", String.valueOf(passiveModeHandler.getMitmPid()));
+                                        p.start().wait();
+                                    } catch (Exception exc) {
+                                        LoggerHolder.get().warn("Exception occurred while terminating mitmproxy child process", exc);
+                                    }
+                                }
                             }
-                            proxy.stop();
+                            mitmLauncher.stop();
                         } catch (Exception ex) {
-                            LoggerHolder.get().warn("MitmproxyJavaサーバーのシャットダウンで例外", ex);
+                            LoggerHolder.get().warn("Exception occurred during mitmproxy server shutdown", ex);
                         }
                     }
                     try {
                         server.stop();
                     } catch (Exception ex) {
-                        LoggerHolder.get().warn("Logbook-Kai HTTPサーバーのシャットダウンで例外", ex);
+                        LoggerHolder.get().warn("Exception occurred during HTTP server shutdown", ex);
                     }
                 }
             } catch (Exception e) {
                 handleException(e);
             }
         } catch (Exception e) {
-            LoggerHolder.get().fatal("Logbook-Kai HTTPサーバーの起動に失敗しました", e);
+            LoggerHolder.get().fatal("Failed to start HTTP server", e);
         }
     }
 
@@ -146,6 +129,12 @@ public final class ProxyServerImpl implements ProxyServerSpi {
                 "ポート " + port + " は既に使用されています。\n" +
                         "設定画面でポート番号を変更するか、他のアプリケーションを終了してください。",
                 null);
+    }
+
+    private static int findAvailablePort() throws IOException {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            return socket.getLocalPort();
+        }
     }
 
     private static void handleException(Exception e) {

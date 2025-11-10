55ee026060e13d151713e095511d0311b4378e10
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2025-11-10 21:05:34 +0900

    chore: 2025年秋イベント後段作戦E5の海域札に暫定対応

--- src/main/java/logbook/internal/SeaAreaBanner.java
+++ src/main/java/logbook/internal/SeaAreaBanner.java
@@ -11,9 +11,6 @@ final class SeaAreaBanner {
 
     private static final int IMAGE_NUMBER_BASE_OFFSET = 12;
     private static final int IMAGE_NUMBER_MULTIPLIER = 2;
-    // 以下の定数は、計算で求められない例外的なパターンに適用する
-    private static final int IMAGE_NUMBER_AREA_1 = 4;  // 改R4計画艦隊
-    private static final int IMAGE_NUMBER_AREA_10 = 5; // 礼号作戦部隊
 
     /**
      * 出撃札の画像パスを取得します
@@ -22,15 +19,22 @@ final class SeaAreaBanner {
      * @return 出撃札の画像パス。存在しない場合はnull
      */
     static Path getJoinBannerPath(int area) {
-        if (area < 1 || area > 10) {
+        if (area < 1 || area > 14) {
             return null;
         }
 
         // 2025年秋イベント後段作戦バージョン
+        // 不規則な番号に対しては定数化も行わない
         final int imageNumber = switch (area) {
-            case 1 -> IMAGE_NUMBER_AREA_1;
-            case 10 -> IMAGE_NUMBER_AREA_10;
-            default -> IMAGE_NUMBER_BASE_OFFSET + area * IMAGE_NUMBER_MULTIPLIER; // 札2~9 -> 14, 16, ..., 28, 30
+            case 1 -> 4;    // 改R4計画艦隊
+            case 10 -> 5;   // 礼号作戦部隊
+            // 以下4種のE5札は合っているか未確認
+            case 11 -> 11;  // 第百四戦隊
+            case 12 -> 7;   // 横須賀防備戦隊
+            case 13 -> 9;   // 連合艦隊
+            case 14 -> 13;  // 決戦艦隊
+            // 札2~9 -> 14, 16, ..., 28, 30
+            default -> IMAGE_NUMBER_BASE_OFFSET + area * IMAGE_NUMBER_MULTIPLIER;
         };
 
         return Paths.get("common", COMMON_EVENT, String.format("%s_%d.png", COMMON_EVENT, imageNumber));

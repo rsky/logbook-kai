d7b38442588bd8b69ef3755a931143efbb190026
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2026-05-04 21:24:54 +0900

    chore: 非アプリ版に同梱するスクリプトの起動オプションを更新

--- dist-includes/launch.bat
+++ dist-includes/launch.bat
@@ -1,2 +1,2 @@
-SET JVM_OPT="-XX:MaxMetaspaceSize=256M --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED"
-START javaw %JVM_OPT% -Djavafx.allowjs=true -Dlogbook_kai.config_dir=.\config -Dlogbook_kai.data_dir=. -jar logbook-kai.jar
+SET JVM_OPT="-XX:MaxMetaspaceSize=256M --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED --enable-native-access=javafx.graphics,ALL-UNNAMED"
+START javaw %JVM_OPT% -Dlogbook_kai.config_dir=.\config -Dlogbook_kai.data_dir=. -jar logbook-kai.jar
--- dist-includes/launch.sh
+++ dist-includes/launch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-JVM_OPT="-XX:MaxMetaspaceSize=256M --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED"
-java $JVM_OPT -Djavafx.allowjs=true -Dlogbook_kai.config_dir=./config -Dlogbook_kai.data_dir=. -jar logbook-kai.jar
+JVM_OPT="-XX:MaxMetaspaceSize=256M --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED --enable-native-access=javafx.graphics,ALL-UNNAMED"
+java $JVM_OPT -Dlogbook_kai.config_dir=./config -Dlogbook_kai.data_dir=. -jar logbook-kai.jar
